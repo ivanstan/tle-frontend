2d0fb2e7cf5eab752f55710aadbeb087d6c5167a
Ivan Stanojevic <ivanstan@gmail.com>
2021-02-27 21:17:56 +0100

    added tle client

--- src/components/TleDisplay.tsx
+++ src/components/TleDisplay.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Tle } from "tle-client";
+import { CopyButton } from "./CopyButton";
 
-export class TleDisplay extends React.Component<any, any> {
+interface TleDisplayPropsInterface {
+  data: Tle;
+}
+
+export class TleDisplay extends React.Component<TleDisplayPropsInterface, any> {
 
   render() {
     const { data } = this.props;
 
     return (
-      <div className="code">
-        <div>{data.line1}</div>
-        <div>{data.line2}</div>
+      <div className="code tle-display d-flex justify-content-between">
+        <div>
+          <div className="name">
+            <span className={'element'}>{data.name}</span>
+          </div>
+          <div>{data.line1}</div>
+          <div>{data.line2}</div>
+        </div>
+        <div className="align-self-center">
+          <CopyButton value={data.line1 + "\n" + data.line2}/>
+        </div>
       </div>
     );
   }
